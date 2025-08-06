97ae10e328d78f9ef60f53432e55bc00fcb28390
Benoit Miller <benoit.miller@gmail.com>
2025-08-06 12:27:41 -0400

    FIxing errors...

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -3,11 +3,27 @@ Import-Module -Name Terminal-Icons
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-Import-Module PSReadLine
+function Test-ModuleExists {
+    param (
+        [string]$ModuleName
+    )
+    return [bool](Get-Module -ListAvailable -Name $ModuleName)
+}
+
+if (Test-ModuleExists -ModuleName "PSReadLine")
+    Import-Module PSReadLine
+
 Set-PSReadLineOption -PredictionSource History
 
 Set-Alias Open Start
 Set-Alias ll dir
 
+function Test-PwshExists {
+    return [bool](Get-Command pwsh -ErrorAction SilentlyContinue)
+}
+
 $Env:EDITOR = "code"
-$Env:SHELL = "pwsh -NoLogo"
+if (Test-PwshExists)
+    $Env:SHELL = "pwsh -NoLogo"
+else
+    $Env:SHELL = "powershell -NoLogo"
